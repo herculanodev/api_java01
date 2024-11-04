6fb704f8e03c7206d9165affbba40de4d8d98128
Daniel Rodrigues Herculano <daniel.herculano@cl.df.gov.br>
2024-11-04 17:26:55 -0300

    subindo metodo get

--- src/main/java/med/voll/api/endereço/Endereço.java
+++ src/main/java/med/voll/api/endereço/Endereço.java
@@ -3,28 +3,32 @@ package med.voll.api.endereço;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Endereço {
-
 
     private String logradouro;
     private String bairro;
     private String cep;
-    private  String cidade;
+    private String cidade;
     private String uf;
     private String complemento;
     private String numero;
 
+    // Construtor que recebe DadosEndereço
     public Endereço(DadosEndereço dados) {
-            this.logradouro = dados.logradouro();
-            this.complemento = dados.complemento();
-            this.uf = dados.uf();
-            this.cidade = dados.cidade();
-            this.bairro = dados.bairro();
-            this.numero = dados.numero();
-            this.cep = dados.cep();
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.complemento = dados.complemento();
+        this.numero = dados.numero();
     }
 }
